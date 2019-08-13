debaa74f129d8e00e2b53a7dce02489ad4406a91
utkarsh <utkarsh.tiwari@somaiya.edu>
2019-08-13 11:22:26 +0530

    added displayBooks

--- js/main.js
+++ js/main.js
@@ -33,6 +33,15 @@ class UI {
 
     static addBookToList(book) {
         const list = document.querySelector('#book-list');
+
+        const row = document.createElement('tr');
+
+        row.innerHTML = `
+        <td>${book.title}</td>
+        <td>${book.author}</td>
+        <td>${book.isbn}</td>
+        <td><a href="#" class = "btn btn-danger btn-sm delete">X</a></td>
+        `
     }
 }
 
