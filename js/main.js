99291196e5ef86b02d6c79d8614475b5cba86ac1
utkarsh <utkarsh.tiwari@somaiya.edu>
2019-08-13 11:09:21 +0530

    added displayBooks

--- js/main.js
+++ js/main.js
@@ -12,6 +12,23 @@ class Book {
 
 // UI Class: Handle UI Class
 
+class UI {
+    static displayBooks() {
+        const storedBooks = [{
+                title: 'John Doe',
+                author: 'Jane Doe',
+                isbn: '123456789'
+            },
+            {
+                title: 'Vroom Vroom',
+                author: 'Lallanop',
+                isbn: '234237493'
+            }
+        ];
+        
+    }
+}
+
 // Store Class: Handles Storage
 
 // Event: Display Books
